04f287a14323a0c6f7b4907d4c589f7b3e0bc0c6
Trung Tran <trungtran.chunml@gmail.com>
2020-12-06 21:52:39 +0900

    Init and configure Typescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const main = (): void => {
+  const app = express();
+
+  app.get("/", (_, res) => {
+    res.json({status: "success"})
+  });
+
+  app.listen(8000, () => {
+    console.log("server is listening at http://localhost:8000");
+  })
+}
+
+main();
