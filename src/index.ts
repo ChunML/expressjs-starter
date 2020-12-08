4f1e125a04e53f695013f5bf29b6c500c348fd4d
Trung Tran <trungtran.chunml@gmail.com>
2020-12-08 13:41:23 +0900

    Initialize expressjs with Typescript, Eslint and Prettier

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ const main = (): void => {
   const app = express();
 
   app.get("/", (_, res) => {
-    res.json({status: "success"})
+    res.json({ status: "success" });
   });
 
   app.listen(8000, () => {
-    console.log("server is listening at http://localhost:8000");
-  })
-}
+    console.log(
+      "server is listening at http://localhost:8000. Check it outtttttt!"
+    );
+  });
+};
 
 main();
